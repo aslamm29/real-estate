e723b4faae6ee163e7bd944cc48f93f040b55100
aslamm <aslamm.anas29@gmail.com>
2019-11-10 23:24:46 -0500

    App Setup

---
+++ src/components/realEstate.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+class RealEstate extends Component {
+    constructor(){
+        super()
+        this.state={
+            name: "Aslam"
+        }
+    }
+
+    render(){
+        return(<div>Real Estate</div>)
+    }
+}
+
+export default RealEstate
