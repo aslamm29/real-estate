96705960d452ffa77e5d5554b326549efe37d811
aslamm <aslamm.anas29@gmail.com>
2019-11-15 13:31:19 -0500

    creating data and importing in state of App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import RealEstate from "./components/realEstate"
 import Header from "./components/Header"
 import Filter from "./components/Filter"
 import Listings from "./components/Listings"
+import listingsData from "./components/data/listingsData.js"
 
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <section id="content-area">
-        <Filter />
-        <Listings />
-      </section>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state ={
+      name: 'Aslam',
+      listingsData
+    }
+  }
+
+  render(){
+    console.log(this.state.listingsData)
+    return (
+      <div>
+        <Header />
+        <section id="content-area">
+          <Filter />
+          <Listings />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/data/listingsData.js
@@ -0,0 +1,102 @@
+var listingsData = [
+    {
+        address: '20-34 grand ave',
+        city: 'Toronto',
+        state: 'ON',
+        rooms: 3,
+        price: 220000,
+        floorSpace: 2000,
+        extras: [
+            'elevator',
+            'gym'
+        ],
+        homeType: 'Apartment',
+        image: 'https://media.equityapartments.com/images/c_crop,x_0,y_0,w_1920,h_1080/c_fill,w_1920,h_1080/q_80/4206-28/the-kelvin-apartments-exterior.jpg'
+    },
+    {
+        address: '158 grand ave',
+        city: 'Victoria',
+        state: 'BC',
+        rooms: 2,
+        price: 24666,
+        floorSpace: 1430,
+        extras: [
+            'elevator',
+            'gym'
+        ],
+        homeType: 'Apartment',
+        image: 'https://pix10.agoda.net/hotelImages/652/65241/65241_15060313050028323977.jpg?s=1024x768'
+    },
+    {
+        address: '20-34 grand ave',
+        city: 'Guelph',
+        state: 'ON',
+        rooms: 1,
+        price: 345355,
+        floorSpace: 2400,
+        extras: [
+            'elevator',
+            'gym'
+        ],
+        homeType: 'Apartment',
+        image: 'https://cdn.vox-cdn.com/thumbor/TWSwK6KYaGquOyd-otLHBDsIODw=/0x0:960x641/1200x800/filters:focal(404x245:556x397)/cdn.vox-cdn.com/uploads/chorus_image/image/52676769/will5.0.jpg'
+    },
+    {
+        address: '20-34 grand ave',
+        city: 'Sydney',
+        state: 'NB',
+        rooms: 3,
+        price: 80000,
+        floorSpace: 1000,
+        extras: [
+            'elevator',
+            'gym'
+        ],
+        homeType: 'Apartment',
+        image: 'https://images1.apartments.com/i2/P0s3lHrhIIPXfyhkVz0wMZXOqFj2m3Rg_Pa-OdKQIQk/111/the-500-atlanta-ga-refined-apartment-living---elite-upgrade.jpg'
+    },
+    {
+        address: '20-34 grand ave',
+        city: 'Waterloo',
+        state: 'ON',
+        rooms: 3,
+        price: 603000,
+        floorSpace: 3000,
+        extras: [
+            'elevator',
+            'gym'
+        ],
+        homeType: 'Condo',
+        image: 'https://www.onni.com/wp-content/uploads/2016/11/Rental-Apartment-Page-new-min.jpg'
+    },
+    {
+        address: '20-34 grand ave',
+        city: 'North York',
+        state: 'ON',
+        rooms: 2,
+        price: 700000,
+        floorSpace: 2900,
+        extras: [
+            'elevator',
+            'pool'
+        ],
+        homeType: 'Apartment',
+        image: 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/ixPdSl7.SV8A/v1/1200x-1.jpg'
+    },
+    {
+        address: '20-34 grand ave',
+        city: 'Etobicoke',
+        state: 'ON',
+        rooms: 1,
+        price: 150000,
+        floorSpace: 2000,
+        extras: [
+            'elevator',
+            'gym'
+        ],
+        homeType: 'Apartment',
+        image: 'https://www.parisperfect.com/g/apartment-hero-images/hi_1-languedoc-apartment.jpg'
+    }
+]
+
+export default listingsData;
