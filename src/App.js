74486b7ee639623172c926a8895c81e93e9d8909
aslamm <aslamm.anas29@gmail.com>
2019-11-16 00:03:02 -0500

    Adding functionality to the filter form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import RealEstate from "./components/realEstate"
 import Header from "./components/Header"
 import Filter from "./components/Filter"
 import Listings from "./components/Listings"
@@ -11,17 +10,36 @@ class App extends Component {
     super()
     this.state ={
       name: 'Aslam',
-      listingsData
+      listingsData,
+      min_price: 0,
+      max_price: 10000000,
+      min_floor_space: 0,
+      max_floor_space: 50000,
+      elevator: false,
+      finished_basement: false,
+      gym: false,
+      swimming_pool: false
     }
+
+    this.change = this.change.bind(this)
+  }
+
+  change(event){
+    let name = event.target.name
+    let value = (event.target.type === 'checkbox') ? event.target.checked : event.target.value
+    this.setState({
+      [name]: value
+    }, () => {
+      console.log(this.state)
+    })
   }
 
   render(){
-    console.log(this.state.listingsData)
     return (
       <div>
         <Header />
         <section id="content-area">
-          <Filter />
+          <Filter change={this.change} globalState={this.state} />
           <Listings listingsData={this.state.listingsData} />
         </section>
       </div>
