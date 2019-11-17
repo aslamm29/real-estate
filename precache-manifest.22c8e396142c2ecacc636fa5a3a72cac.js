a9277ce42fb320b53a440179e73fd7897a64431f
aslamm <aslamm.anas29@gmail.com>
2019-11-17 15:32:57 -0500

    Updates

---
+++ precache-manifest.22c8e396142c2ecacc636fa5a3a72cac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7e461e01d52ceb2a54008e47f2209d",
+    "url": "/real-estate/index.html"
+  },
+  {
+    "revision": "105ab3486243a9892eb1",
+    "url": "/real-estate/static/css/main.73e7c3a5.chunk.css"
+  },
+  {
+    "revision": "ee9495c908a621dd3467",
+    "url": "/real-estate/static/js/2.3ba8e864.chunk.js"
+  },
+  {
+    "revision": "105ab3486243a9892eb1",
+    "url": "/real-estate/static/js/main.bd63f8e3.chunk.js"
+  },
+  {
+    "revision": "0dbc554efb80e3189e17",
+    "url": "/real-estate/static/js/runtime-main.53511fb1.js"
+  }
+]);
